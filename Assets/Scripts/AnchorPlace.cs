6749ecd8b347f6129b44ba8d0ca8c9723e6ecdfc
Hyunsoo Park <smwater130@naver.com>
2022-11-01 19:15:06 +0900

    [Fix] 버그 수정

--- Assets/Scripts/AnchorPlace.cs
+++ Assets/Scripts/AnchorPlace.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Google.XR.ARCoreExtensions;
-using UnityEngine.XR.ARSubsystems;
 using UnityEngine.XR.ARFoundation;
 
 public class AnchorPlace : MonoBehaviour
@@ -11,22 +10,36 @@ public class AnchorPlace : MonoBehaviour
     public GameObject Player;
 
     private ARAnchorManager _arAnchorManager;
-    private AREarthManager _arEarthManager;
+    private bool _isOk = false;
 
     private void Awake()
     {
         _arAnchorManager = GetComponent<ARAnchorManager>();
+    }
 
-        GeospatialPose geospatialPose = _arEarthManager.Convert(new Pose(Player.transform.position, Player.transform.rotation));
+    private void Update()
+    {
+        AREarthManager arEarthManager = new AREarthManager();
 
-        GeoAnchorPlace(37.5398185, 127.1229427, geospatialPose.Altitude, geospatialPose.EunRotation);
-        GeoAnchorPlace(37.5397658, 127.1230607, geospatialPose.Altitude, geospatialPose.EunRotation);
-        GeoAnchorPlace(37.5397393, 127.1231278, geospatialPose.Altitude, geospatialPose.EunRotation);
+        if (!_isOk)
+        {
+            Debug.Log(Player.transform.position);
+
+            GeospatialPose geospatialPose = arEarthManager.Convert(new Pose(Player.transform.position, Player.transform.rotation));
+            
+            Debug.Log(geospatialPose);
+
+            GeoAnchorPlace(37.5398185, 127.1229427, geospatialPose.Altitude, geospatialPose.EunRotation);
+            GeoAnchorPlace(37.5397658, 127.1230607, geospatialPose.Altitude, geospatialPose.EunRotation);
+            GeoAnchorPlace(37.5397393, 127.1231278, geospatialPose.Altitude, geospatialPose.EunRotation);
+
+            _isOk = true;
+        }
     }
 
     private void GeoAnchorPlace(double latitude, double longitude, double altitude, Quaternion eunRotation)
     {
-        ARGeospatialAnchor anchor = ARAnchorManagerExtensions.AddAnchor(_arAnchorManager, latitude, longitude, altitude, eunRotation);
+        ARGeospatialAnchor anchor = _arAnchorManager.AddAnchor(latitude, longitude, altitude, eunRotation);
         GameObject marker = Instantiate(MarkerPrefab, anchor.transform);
     }
 }
