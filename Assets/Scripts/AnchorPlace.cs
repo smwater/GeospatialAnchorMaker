23505b95a4b1c1c4e06249df5d0338287578e5cb
Hyunsoo Park <smwater130@naver.com>
2022-11-03 10:42:10 +0900

    [Feat] CameraGeospatialPose를 이용한 고도 구하기 구현
    및 Tracking 상태 아닐 때 예외처리

--- Assets/Scripts/AnchorPlace.cs
+++ Assets/Scripts/AnchorPlace.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Google.XR.ARCoreExtensions;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class AnchorPlace : MonoBehaviour
 {
@@ -14,12 +15,19 @@ public class AnchorPlace : MonoBehaviour
 
     public void Create()
     {
+        if (AREarthManager.EarthTrackingState != TrackingState.Tracking)
+        {
+            Debug.Log("Tracking 상태가 아닙니다. 잠시만 기다려주세요.");
+        }
+
         // 사용자 기기와 유사한 고도
         //GeospatialPose geospatialPose = AREarthManager.Convert(new Pose(Player.transform.position, Player.transform.rotation));
+        // 카메라를 이용한 고도
+        GeospatialPose geospatialPose = AREarthManager.CameraGeospatialPose;
 
-        //GeoAnchorPlace(37.5398185, 127.1229427, geospatialPose.Altitude, geospatialPose.EunRotation);
-        //GeoAnchorPlace(37.5397658, 127.1230607, geospatialPose.Altitude, geospatialPose.EunRotation);
-        //GeoAnchorPlace(37.5397393, 127.1231278, geospatialPose.Altitude, geospatialPose.EunRotation);
+        GeoAnchorPlace(37.5398185, 127.1229427, geospatialPose.Altitude, geospatialPose.EunRotation);
+        GeoAnchorPlace(37.5397658, 127.1230607, geospatialPose.Altitude, geospatialPose.EunRotation);
+        GeoAnchorPlace(37.5397393, 127.1231278, geospatialPose.Altitude, geospatialPose.EunRotation);
     }
 
     private void GeoAnchorPlace(double latitude, double longitude, double altitude, Quaternion eunRotation)
